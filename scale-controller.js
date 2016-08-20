d95131b5a5e74af9ef4103e3cead7ec7b35c8be5
alexwest <alex@localhost>
2016-08-20 04:29:13 +0300

    refactoring

--- scale-controller.js
+++ scale-controller.js
@@ -1,49 +1,61 @@
-(function(window, $){
+(function () {
 	'use strict';
 	
-	if (!$)
+	if (!jQuery)
 		throw new Error('Scale controller requires jQuery.');
 	
-	window.scale = 1;
-	var re = /^([0-9]+|=)x([0-9]+|=)$/;
+	var re = /^([0-9]+)x([0-9]+)$/;
 	
 	$(function(){
-		var $window	= $( window ),
-			$body	= $( window.document.body ),
-			$element= $( '[data-scale]', $body ),
-			config	= $element.attr('data-scale').match(re) || false;
+		var element	= $('[data-scale]')[0],
+			strict	= element.attr('data-scale').match(re) || false,
+			size	= [element.width(),element.height()];
 		
-		var width 	= $element.width(),
-			height	= $element.height();
-		
-		if ($element.length == 1)
-		{
-			if (config)
-			{
-				width	= (config[1] != '=') ? parseInt(config[1]) : width ;
-				height	= (config[2] != '=') ? parseInt(config[2]) : height ;
-			}
-			
-			$element.css({
-				"width": width+'px',
-				"height": height+'px'
-			});
-			
-			var controller = function(){
-				var scaleWidth	= $window.width() / width,
-					scaleHeight = $window.height() / height;
-				
-				var scale = window.scale = (scaleHeight > scaleWidth) ? scaleWidth : scaleHeight,
-					offset = $body.width() - (width * scale);
-				
-				$element.css({
-					"transform": 'scale('+scale+', '+scale+ ')',
-					"margin-left": (offset > 0) ? (offset / 2) : 0
-				});
-			};
-			
-			$window.on('resize', controller) && controller();
+		if (strict !== false) {
+			size[0] = parseInt(strict[1]);
+			size[1] = parseInt(strict[2]);
 		}
+		
+		element.css({
+			"width": size[0],
+			"height": size[1]
+		});
+		
+		var calcScale = function () {
+			var ws = [$(window).width(),$(window).height()];
+			
+			var widthScale	= ws[0] / size[0],
+				heightScale = ws[1] / size[1];
+			
+			var scale = (heightScale > widthScale) ? widthScale : heightScale,
+				offset = ws[0] - (size[0] * scale);
+			
+			return {
+				"scale": scale,
+				"offset": offset
+			}
+		};
+		
+		var handler = function () {
+			var scale = calcScale();
+			
+			element.css({
+				"transform": 'scale('+scale.scale+','+scale.scale+')',
+				"margin-left": (scale.offset > 0) ? (scale.offset / 2) : 0
+			});
+		};
+		
+		Object.defineProperty(window, 'scale', {
+			"get": function () {
+				return calcScale().scale;
+			},
+			"set": function (value) {},
+			"configurable": false,
+			"enumerable": true,
+		});
+		
+		$(window).on('resize', handler);
+		handler();
 	});
 	
-})(window, window.jQuery);
+})();
